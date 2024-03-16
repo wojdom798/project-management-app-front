8acde03f5ab14c7403215cb66f9cedcec100f697
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2024-03-16 20:06:05 +0100

    feat: add 'toggle finished' button to Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import { ITaskProps } from "../types/frontendSpecificTypes";
 
-function Task({ id, name }: ITaskProps)
+function Task({ id, name, isFinished }: ITaskProps)
 {
+    const [isFinishedUI, toggleFinished] = useState<boolean>(isFinished);
+
     return (
-        <li className="task">
-            { name }
+        <li
+            className={
+                isFinishedUI ?
+                "task task--finished" :
+                "task"
+            }
+        >
+            <span>{ name }</span>
+            <button
+                onClick={ () => toggleFinished(!isFinishedUI) }
+            >
+                toggle finished
+            </button>
         </li>
     );
 }
