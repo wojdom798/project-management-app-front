0196df5660440145a99dae7bc8ea41f6f8b4e9e1
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2024-03-26 18:07:17 +0100

    feat: implement rudimentary create new feature form

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, SyntheticEvent } from "react";
 import Feature from "./Feature";
 import { IProjectProps } from "../types/frontendSpecificTypes";
 import { IFeature, ITask } from "../types/sharedTypes";
 
 function Project(props: IProjectProps)
 {
+    const [isCreateNewFeatureFormActive, setIsCreateNewFeatureFormActive] =
+        useState<boolean>(false);
+
+    // new feature form inputs
+    const [nameField, setNameField] = useState<string>("");
+    const [priorityField, setPriorityField] = useState<number>(1);
 
     const getFeatures = () =>
     {
@@ -23,6 +29,43 @@ function Project(props: IProjectProps)
         });
     };
 
+    const handleCreateNewFeatureButtonClick = () =>
+    {
+        setIsCreateNewFeatureFormActive(true);
+    };
+
+    const handleCreateNewFeatureFormSubmit = (event: SyntheticEvent) =>
+    {
+        event.preventDefault();
+        const submitData = {
+            name: nameField,
+            priority: priorityField
+        }
+        console.log(submitData);
+    };
+
+    const handleCreateNewFeatureFormCancel = (event: SyntheticEvent) =>
+    {
+        event.preventDefault();
+        setIsCreateNewFeatureFormActive(false);
+    };
+
+    const handleNewFeatureNameInputChange = (event: SyntheticEvent) =>
+    {
+        const inputValue = (event.target as HTMLInputElement).value;
+        setNameField(inputValue);
+    };
+
+    const handleNewFeaturePriorityInputChange = (event: SyntheticEvent) =>
+    {
+        const inputValue = (event.target as HTMLInputElement).value;
+        let sanitizedValue = parseInt(inputValue);
+        if (!isNaN(sanitizedValue))
+        {
+            setPriorityField(sanitizedValue);
+        }
+    };
+
     return (
         <div className="project">
             <h2 className="project__title">{props.name}</h2>
@@ -33,9 +76,40 @@ function Project(props: IProjectProps)
             </div>
 
             <div className="project__new-feature-button">
-                <button
-                    onClick={ () => { console.log("new feature button clicked") } }
-                >new feature</button>
+
+                { isCreateNewFeatureFormActive ?
+                (
+                    <form>
+                        <h3>Create New Feature</h3>
+
+                        <input
+                            type={"text"}
+                            value={nameField}
+                            placeholder={"name"}
+                            onChange={handleNewFeatureNameInputChange}
+                        />
+
+                        <input
+                            type={"text"}
+                            value={priorityField}
+                            placeholder={"priority (positive number)"}
+                            onChange={handleNewFeaturePriorityInputChange}
+                        />
+
+                        <button
+                            onClick={handleCreateNewFeatureFormSubmit}
+                        >submit</button>
+                        <button
+                            onClick={handleCreateNewFeatureFormCancel}
+                        >cancel</button>
+                    </form>
+                ):(
+                    <button
+                        onClick={handleCreateNewFeatureButtonClick}
+                    >new feature</button>
+                )}
+
+                
             </div>
         </div>
     );
